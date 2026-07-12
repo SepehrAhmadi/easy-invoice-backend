696af6160941ea4673b21a27ec32613cc0becea5
SepehrAhmadi <ahmadisepehr75@gmail.com>
2026-07-12 11:44:22 +0330

    Add username to notification responses

--- services/notification/notificationService.js
+++ services/notification/notificationService.js
@@ -1,4 +1,5 @@
 const Notification = require("../../model/Notification");
+const User = require("../../model/User");
 const notificationMessages = require("../../language/notification/index");
 const { getIO } = require("../../config/socket");
 const logger = require("../../utils/logger");
@@ -8,22 +9,32 @@ const getNotifications = async () => {
   const notifications = await Notification.find()
     .sort({ createdDate: -1 })
     .exec();
+
   if (!notifications) {
     return { notifications: [] };
   }
 
-  return notifications.map((item) => ({
-    userId: item.userId,
-    action: item.action,
-    type: item.type,
-    enTitle: item.title.en,
-    faTitle: item.title.fa,
-    enMessage: item.message.en,
-    faMessage: item.message.fa,
-    date: moment(item.date).format("M/D/YYYY HH:mm"),
-    localDate: item.localDate,
-    createdData: item.createdDate,
-  }));
+  const notificationsData = await Promise.all(
+    notifications.map(async (item) => {
+      const user = await User.findById(item.userId).select("username").lean();
+
+      return {
+        userId: item.userId,
+        user: user?.username,
+        action: item.action,
+        type: item.type,
+        enTitle: item.title.en,
+        faTitle: item.title.fa,
+        enMessage: item.message.en,
+        faMessage: item.message.fa,
+        date: moment(item.date).format("M/D/YYYY HH:mm"),
+        localDate: item.localDate,
+        createdDate: item.createdDate,
+      };
+    }),
+  );
+
+  return notificationsData;
 };
 
 const create = async ({ userId, action, type, data }) => {
@@ -38,7 +49,7 @@ const create = async ({ userId, action, type, data }) => {
     const localDate = moment(now).locale("fa").format("YYYY/MM/DD HH:mm");
 
     const notification = await Notification.create({
-      userId: userId,
+      userId,
       action,
       type,
       title: notificationText.title,
@@ -48,8 +59,11 @@ const create = async ({ userId, action, type, data }) => {
       createdDate: now,
     });
 
+    const user = await User.findById(userId).select("username").lean();
+
     const responseNotification = {
       userId: notification.userId,
+      username: user?.username,
       action: notification.action,
       type: notification.type,
       enTitle: notification.title.en,
@@ -58,11 +72,10 @@ const create = async ({ userId, action, type, data }) => {
       faMessage: notification.message.fa,
       date: moment(notification.date).format("M/D/YYYY HH:mm"),
       localDate,
-      createdData: notification.createdDate,
+      createdDate: notification.createdDate,
     };
-    
+
     const io = getIO();
-    console.log("Sending notification:", notification);
     io.emit("notification", responseNotification);
 
     return notification;
