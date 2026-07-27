9dbbea1babb51cca327d801d66abec5e90a87f60
SepehrAhmadi <ahmadisepehr75@gmail.com>
2026-07-27 12:26:43 +0330

    Add Vercel frontend URL to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://easy-invoice-backend-nv19.onrender.com",
+  "https://easy-invoice-frontend-green.vercel.app",
   "http://localhost:8081",
   "http://localhost:3000",
   "http://localhost:3001",
